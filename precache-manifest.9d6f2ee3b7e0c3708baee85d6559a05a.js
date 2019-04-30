8146ffecd1194f7563c713dcf2d9cb2b7b04e449
Rima Faiqoh Augustine <augustine@campus.tu-berlin.de>
2019-04-30 15:44:59 +0200

    Updates

---
+++ precache-manifest.9d6f2ee3b7e0c3708baee85d6559a05a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6cb0da383702c3a35b52",
+    "url": "/static/js/main.da15b7c9.chunk.js"
+  },
+  {
+    "revision": "42e7dbc33c2561a277a0",
+    "url": "/static/js/2.f20fd9a2.chunk.js"
+  },
+  {
+    "revision": "6cb0da383702c3a35b52",
+    "url": "/static/css/main.405728b1.chunk.css"
+  },
+  {
+    "revision": "77e12bc5218963777407beb6c774ae4d",
+    "url": "/index.html"
+  }
+];
